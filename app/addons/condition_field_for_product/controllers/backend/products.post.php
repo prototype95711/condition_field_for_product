d09ebe5a6d484a1c54a6e0373ef076fdadb61fa7
Gleb Perfiliev <prototype955@yandex.ru>
2020-05-08 23:21:29 +0400

    Added .post-controller for add condition field in products multi-upgrade

---
+++ app/addons/condition_field_for_product/controllers/backend/products.post.php
@@ -0,0 +1,58 @@
+<?php
+/***************************************************************************
+*                                                                          *
+*   (c) 2004 Vladimir V. Kalynyak, Alexey V. Vinokurov, Ilya M. Shalnev    *
+*                                                                          *
+* This  is  commercial  software,  only  users  who have purchased a valid *
+* license  and  accept  to the terms of the  License Agreement can install *
+* and use this program.                                                    *
+*                                                                          *
+****************************************************************************
+* PLEASE READ THE FULL TEXT  OF THE SOFTWARE  LICENSE   AGREEMENT  IN  THE *
+* "copyright.txt" FILE PROVIDED WITH THIS DISTRIBUTION PACKAGE.            *
+****************************************************************************/
+
+use Tygh\Registry;
+
+if (!defined('BOOTSTRAP')) { die('Access denied'); }
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    return array(CONTROLLER_STATUS_OK);
+}
+
+if ($mode == 'manage') {
+
+    $selected_fields = Tygh::$app['view']->getTemplateVars('selected_fields');
+
+    $selected_fields[] = array('name' => '[data][product_condition_value]', 'text' => __('product_condition_value'));
+
+    Tygh::$app['view']->assign('selected_fields', $selected_fields);
+
+} elseif ($mode == 'm_update') {
+
+    $selected_fields = $_SESSION['selected_fields'];
+
+    $field_groups = Tygh::$app['view']->getTemplateVars('field_groups');
+    $filled_groups = Tygh::$app['view']->getTemplateVars('filled_groups');
+    $field_names = Tygh::$app['view']->getTemplateVars('field_names');
+
+    if (!empty($selected_fields['data']['product_condition_value'])) {
+        $field_groups['S']['product_condition_value'] = array (
+            'name' => 'products_data',
+            'variants' => array (
+                'destroyed' => 'destroyed',
+                'poor' => 'poor',
+                'good' => 'good',
+                'average' => 'average',
+                'excellent' => 'excellent'
+            ),
+        );
+        $filled_groups['S']['product_condition_value'] = __('product_condition_value');
+        unset($field_names['product_condition_value']);
+    }
+
+    Tygh::$app['view']->assign('field_groups', $field_groups);
+    Tygh::$app['view']->assign('filled_groups', $filled_groups);
+    Tygh::$app['view']->assign('field_names', $field_names);
+
+}
